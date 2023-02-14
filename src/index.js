101ed5fea8cc48af46779070df45def24ee9bdce
Nemirich15 <nemirka15@gmail.com>
2023-02-14 17:49:22 +0300

    feat: brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
+
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  //const openBrackets = ['(', '{', '[', '|'];
+  const bracketsConfig = [
+    ['(', ')'],
+    ['{', '}'],
+    ['[', ']'],
+    ['|', '|'],
+  ];
+  
+  
+  let stack = [];
+
+  for( let i = 0; i < str.length; i++){
+    let currentSymbol = str[i];
+
+    //if(openBrackets.includes(currentSymbol)){
+    for(let i = 0; i < bracketsConfig.length; i++){
+      if(bracketsConfig[i][0].includes(currentSymbol)){
+      stack.push(currentSymbol);
+    } else {
+      if(stack.length === 0) {
+        return false;
+      }
+      let topElement =stack[stack.length - 1];
+
+      
+
+        if( bracketsConfig[i][0] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  
+  
 }
